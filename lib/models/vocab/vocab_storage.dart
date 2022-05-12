2a90571a0f65d9540ee13e4d6ddcb2a9a2d266cf
owen-hwlee <hwolee@connect.ust.hk>
2022-05-13 00:51:00 +0800

    Create vocab_storage.dart

---
+++ lib/models/vocab/vocab_storage.dart
@@ -0,0 +1,10 @@
+import 'package:comp4521_gp4_accelyst/models/vocab/vocab.dart';
+import 'package:comp4521_gp4_accelyst/utils/services/local_storage_service.dart';
+
+class VocabStorage extends StorageService {
+  VocabStorage() {
+    super.initialize();
+    datapath = "vocab";
+    filename = "vocabs.json";
+  }
+}
