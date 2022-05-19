dbebbf73c730068187cc1f3e19950100e3a63ee9
owen-hwlee <hwolee@connect.ust.hk>
2022-05-19 20:44:05 +0800

    Update vocab_storage.dart

--- lib/models/vocab/vocab_storage.dart
+++ lib/models/vocab/vocab_storage.dart
@@ -1,11 +1,16 @@
-import 'package:comp4521_gp4_accelyst/models/vocab/vocab.dart';
 import 'package:comp4521_gp4_accelyst/utils/services/local_storage_service.dart';
 
 class VocabStorage extends StorageService {
-  VocabStorage() {
-    super.initialize(
-      datapath: "vocab",
-      filename: "vocabs.json",
-    );
+  VocabStorage(String filenameNoExt, {void Function()? callback}) {
+    super
+        .initialize(
+      datapath: "vocab-list-data",
+      filename: "$filenameNoExt.json",
+    )
+        .then((_) {
+      if (callback != null) {
+        callback();
+      }
+    });
   }
 }
